35e6215722ad8bac4eb2361d5ad7fd09cce53e80
Roth, Corinne <corinne.roth@pnnl.gov>
2020-02-10 11:51:43 -0800

    creating benchmark tracking report pdf script

---
+++ scripts/bmk_tracking_pdf.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 11:29:43 2020
+
+@author: barn553
+"""
+
+import argparse
+import logging
+import pprint
+import os
+import sys
+from fpdf import FPDF
+import collections as co
+import bmk_plotting
+import benchmark_postprocessing as bmpp
+import make_dataframe as md
